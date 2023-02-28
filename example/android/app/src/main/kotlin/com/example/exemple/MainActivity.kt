2e4c3b5dbaad708208c8d475a563af653c9bbe5b
linkkader <59131542+linkkader@users.noreply.github.com>
2023-02-28 18:42:39 +0100

    v2 success

--- example/android/app/src/main/kotlin/com/example/exemple/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.exemple
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- test/easy_downloader_test.dart
+++ test/easy_downloader_test.dart
@@ -1,21 +1,16 @@
-import 'dart:async';
 import 'package:easy_downloader/easy_downloader.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-var completer = Completer();
+import 'package:test/test.dart';
 
 void main() {
-  test('adds one to input values', () async {
-    //500KB
-    var url =
-        "https://www.learningcontainer.com/download/sample-mp4-video-file-download-for-testing/?wpdmdl=2727&refresh=62dca61b7ad061658627611";
-    //100MB
-    url = "https://speed.hetzner.de/100MB.bin";
-    url =
-        "https://raw.githubusercontent.com/yourkin/fileupload-fastapi/a85a697cab2f887780b3278059a0dd52847d80f3/tests/data/test-10mb.bin";
-    (url);
-    EasyDownloader();
-    //EasyDownloader().download(url);
-    await completer.future;
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
   });
 }
